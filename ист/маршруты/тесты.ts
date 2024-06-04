264743672e0faf685d815ec378138a3d84a92e56
TvSpichkin <ericlen@mail.ru>
2024-06-04 11:38:16 +0300

    Перевод на 16

---
+++ ист/маршруты/тесты.ts
@@ -0,0 +1,15 @@
+import экспресс, {Request, Response, Router} from "express";
+import {типБД} from "../бд/бд";
+
+
+export function полМаршТестов(бд: типБД): Router {
+    const марш = экспресс.Router();
+
+    марш.delete(encodeURI("/данные"), (запр: Request, отв: Response) => {
+        бд.частицы = [];
+
+        отв.sendStatus(204);
+    });
+
+    return марш;
+}
